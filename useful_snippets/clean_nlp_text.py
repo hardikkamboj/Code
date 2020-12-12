b4b3c71fc78986af66deb4dc4573fcce7cb2ce72
hardikkamboj <53142482+hardikkamboj@users.noreply.github.com>
2020-12-12 11:24:00 +0530

    Update clean_nlp_text.py

--- useful_snippets/clean_nlp_text.py
+++ useful_snippets/clean_nlp_text.py
@@ -1,1 +1,11 @@
+# clearning corpus, which contains words only. 
 cleaned_lemmatized_tokens = [lemmatizer.lemmatize(word.lower()) for word in word_tokenize(re.sub(r'([^\s\w]|_)+', ' ', text))]
+
+
+
+# clearning sentences from df 
+# stop_words = stopwords.words('english') # 179 elements
+
+df['cleaned_text'] = df['text'].apply(lambda x : ' '.join([lemmatizer.lemmatize(word.lower()) \
+    for word in word_tokenize(re.sub(r'([^\s\w]|_)+', ' ', str(x))) if word.lower() not in stop_words]))
+
