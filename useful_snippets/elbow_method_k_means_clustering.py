ef9a142ee80fa1f371243a3234308d081ea6ffcd
hardikkamboj <53142482+hardikkamboj@users.noreply.github.com>
2020-12-12 12:01:14 +0530

    Create elbow_method_k_means_clustering.py

---
+++ useful_snippets/elbow_method_k_means_clustering.py
@@ -0,0 +1,24 @@
+# to choose the optimal number of clusters for k means clustering 
+
+from scipy.spatial.distance import cdist
+from sklearn.cluster import KMeans
+
+
+#Using Elbow method to obtain the number of clusters
+distortions = []
+K = range(1,6)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k)
+    kmeanModel.fit(tfidf_df)
+    distortions.append(sum(np.min(cdist(tfidf_df, kmeanModel.cluster_centers_, 'euclidean'), \
+                                  axis=1)) / tfidf_df.shape[0])
+
+
+# Plot the elbow
+plt.plot(K, distortions, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal number of clusters')
+plt.show()
+#FROM THIS PLOT SELECT K WEHRE THE PLOT HAS STEEPEST SLOPE i.e. 2
+
